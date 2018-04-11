6d1d34557395e63565bda4a0a636c6fabed56715
lubo <npeasy@163.com>
2018-04-11 14:31:53 +0800

    asset bundle 的依赖关系不支持动态添加

--- CoreAsset.php
+++ CoreAsset.php
@@ -5,59 +5,28 @@
  * Time: 下午10:44
  */
 
-namespace  anxu;
+namespace anxu;
 
 use yii\web\AssetBundle;
 
 
 class CoreAsset extends AssetBundle
 {
-
-    /**
-     * [$sourcePath description]
-     * @var string
-     */
     public $sourcePath = '@vendor/bower/simditor';
 
     public $uploader = false;
 
-
-    /**
-     * [$css description]
-     * @var array
-     */
     public $css = [
         "styles/simditor.css"
     ];
 
-    /**
-     * [$js description]
-     * @var array
-     */
     public $js = [
         "lib/simditor.js"
     ];
 
-    /**
-     * [$depends description]
-     * @var array
-     */
     public $depends = [
         'anxu\ModuleAsset',
-        'anxu\HotkeysAsset'
+        'anxu\HotkeysAsset',
+        'anxu\UploaderAsset',
     ];
-    
-    /**
-     * @inheritdoc
-     */
-    public function registerAssetFiles($view)
-    {
-
-        if($this->uploader){
-            $this->depends[] = 'anxu\UploaderAsset';
-        }
-
-        parent::registerAssetFiles($view);
-    }
-
 }
